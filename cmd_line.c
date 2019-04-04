b03def943cf5cd594b2fb9443c4db8231b91daf0
mathcode00 <bruno.m.cebola@gmail.com>
2019-04-04 17:01:06 +0100

    kkk

--- cmd_line.c
+++ cmd_line.c
@@ -4,17 +4,37 @@
 #include <time.h>     //usada para se poder calcular o tempo
 #include <ctype.h>    //toupper and tolower functions
 #include <unistd.h>   //permite usar a funcao sleep
-#include <termios.h>  //permite usar a funcao tcflush
 #include <stdio.h>    //funçoes de ficheiros
+#include <dirent.h>
+#include <limits.h>
 
-int main() {
-  FILE *fptr;
-  
-  fptr = fopen("myfile.txt", "w");
-  if (fptr == NULL) {
-    printf("Error opening file.");
-    return -1;
-  }
-  fclose(fptr);
-  return 0;
+int main()
+{
+
+  char cwd[PATH_MAX];
+  char dir[]="\0";
+  struct dirent *de;  // Pointer for directory entry
+  // opendir() returns a pointer of DIR type.
+  DIR *dr;
+
+    while(1){
+      if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("%s\n", cwd);
+        printf("$ ");
+        gets(dir);
+        dr = opendir(dir);
+
+        if (dr == NULL)  // opendir returns NULL if couldn't open directory
+        {
+            printf("Could not find directory\n");
+        }
+
+        while ((de = readdir(dr)) != NULL)
+                printf("%s\n", de->d_name);
+
+        closedir(dr);
+      }
+    }
+
+    return 0;
 }
