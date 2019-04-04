bf9d3e3a7108c86d03628223eb7a29b994906d6a
mathcode00 <bruno.m.cebola@gmail.com>
2019-04-04 16:33:59 +0100

    aaa

--- cmd_line.c
+++ cmd_line.c
@@ -1,1 +1,20 @@
-aaaa
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>   //funcoes de strings
+#include <time.h>     //usada para se poder calcular o tempo
+#include <ctype.h>    //toupper and tolower functions
+#include <unistd.h>   //permite usar a funcao sleep
+#include <termios.h>  //permite usar a funcao tcflush
+#include <stdio.h>    //funçoes de ficheiros
+
+int main() {
+  FILE *fptr;
+  
+  fptr = fopen("myfile.txt", "w");
+  if (fptr == NULL) {
+    printf("Error opening file.");
+    return -1;
+  }
+  fclose(fptr);
+  return 0;
+}
